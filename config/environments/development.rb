0f19472d331f95ca58f79c8638886e8f8a82d8db
Ubuntu <ubuntu@ip-172-31-8-48.ec2.internal>
2022-04-18 07:16:23 +0000

    edited development and production environments for mailer actions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,17 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'evening-dusk-08297.herokuapp.com',
+      user_name:            'user@evening-dusk-08297.herokuapp.com',
+      password:             'password',
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,19 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   
   # this is a line that is added separately so that mailer can work in all environments
-  config.action_mailer.default_url_options = { :host => "https://evening-dusk-08297.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "https://evening-dusk-08297.herokuapp.com" }
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'evening-dusk-08297.herokuapp.com',
+      user_name:            'user@evening-dusk-08297.herokuapp.com',
+      password:             'password',
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
