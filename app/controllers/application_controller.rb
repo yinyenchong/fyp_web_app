881f1b86ba2d83d332dc3d5acaf44ab0d4a0f054
Ubuntu <ubuntu@ip-172-31-4-29.ec2.internal>
2022-06-23 07:45:29 +0000

    minor visual changes again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,16 @@ class ApplicationController < ActionController::Base
   
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
     
+  around_filter :set_time_zone
+
+  def set_time_zone
+    if logged_in?
+      Time.use_zone(current_user.time_zone) { yield }
+    else
+      yield
+    end
+  end
+      
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
