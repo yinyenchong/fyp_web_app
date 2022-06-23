3b36bdd590415c082f52a4c56a0eb2610d2b7936
Ubuntu <ubuntu@ip-172-31-4-29.ec2.internal>
2022-06-23 07:48:58 +0000

    removed time zone config so heroku could function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,6 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-    
-  around_filter :set_time_zone
-
-  def set_time_zone
-    if logged_in?
-      Time.use_zone(current_user.time_zone) { yield }
-    else
-      yield
-    end
-  end
       
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
